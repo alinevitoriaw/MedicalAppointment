041a095fecdb54fd3df20dd69f37fa94bea74921
Kelly Costa <102195159+kellybuss@users.noreply.github.com>
2025-07-27 17:18:54 -0300

    Update urls.py
    oi

--- Hbooking/urls.py
+++ Hbooking/urls.py
@@ -4,9 +4,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-#admin.site.site_header = 'Medical Appointment '
-#admin.site.site_title = 'MedicalAppointment '
-#admin.site.index_title = 'Appointment Administration'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
